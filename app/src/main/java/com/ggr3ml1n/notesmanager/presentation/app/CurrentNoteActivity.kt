9a57fce961976f2e1e94c657965c63fe5339acd3
Dinar Subeev <dinars163@gmail.com>
2024-01-09 03:14:32 +0400

    CurrentNoteActivity had been created

---
+++ app/src/main/java/com/ggr3ml1n/notesmanager/presentation/app/CurrentNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.ggr3ml1n.notesmanager.presentation.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ggr3ml1n.notesmanager.R
+
+class CurrentNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_note)
+    }
+}
