e939023a5aa544da5577ea869066287da9b0e4df
Dinar Subeev <dinars163@gmail.com>
2024-01-10 02:30:57 +0400

    As a result of merging branches, an error occurred and the class was not removed. Bug fixed

--- app/src/main/java/com/ggr3ml1n/notesmanager/data/NoteRepositoryImpl.kt
+++
@@ -1,7 +0,0 @@
-package com.ggr3ml1n.notesmanager.data
-
-import com.ggr3ml1n.notesmanager.domain.NoteRepository
-
-class NoteRepositoryImpl(dataBase: MainDataBase) : NoteRepository {
-    val dao = dataBase.getDAO()
-}
