bac2015c2c65c617e02b8105514206c04b1f436e
Dinar Subeev <dinars163@gmail.com>
2024-01-10 00:22:02 +0400

    Created MainApp which has access to database

---
+++ app/src/main/java/com/ggr3ml1n/notesmanager/presentation/app/MainApp.kt
@@ -0,0 +1,8 @@
+package com.ggr3ml1n.notesmanager.presentation.app
+
+import android.app.Application
+import com.ggr3ml1n.notesmanager.data.MainDataBase
+
+class MainApp : Application() {
+    val dataBase by lazy { MainDataBase.getDataBase(this) }
+}
