bcb8e94b761590f9e45d2463925ee02957f25eec
Dinar Subeev <dinars163@gmail.com>
2024-01-10 03:31:22 +0400

    Changed method signatures to the required ones

--- app/src/main/java/com/ggr3ml1n/notesmanager/data/storage/NoteStorage.kt
+++ app/src/main/java/com/ggr3ml1n/notesmanager/data/storage/NoteStorage.kt
@@ -1,12 +1,15 @@
 package com.ggr3ml1n.notesmanager.data.storage
 
+import androidx.lifecycle.LiveData
+import com.ggr3ml1n.notesmanager.data.entities.NoteData
+
 interface NoteStorage {
 
-    suspend fun deleteNote()
+    suspend fun deleteNote(id: Int)
 
-    suspend fun saveNote()
+    suspend fun saveNote(note: NoteData)
 
-    suspend fun updateNote()
+    suspend fun updateNote(note: NoteData)
 
-    suspend fun getNotesByDate()
+    fun getNotesByDate(dateStart: String, dateFinish: String) : LiveData<List<NoteData>>
 }
--- app/src/main/java/com/ggr3ml1n/notesmanager/domain/repository/NoteRepository.kt
+++ app/src/main/java/com/ggr3ml1n/notesmanager/domain/repository/NoteRepository.kt
@@ -1,12 +1,15 @@
 package com.ggr3ml1n.notesmanager.domain.repository
 
+import androidx.lifecycle.LiveData
+import com.ggr3ml1n.notesmanager.domain.entities.NoteDomain
+
 interface NoteRepository {
 
-    suspend fun deleteNote()
+    suspend fun deleteNote(id: Int)
 
-    suspend fun saveNote()
+    suspend fun saveNote(note: NoteDomain)
 
-    suspend fun updateNote()
+    suspend fun updateNote(note: NoteDomain)
 
-    suspend fun getNotesByDate()
+    fun getNotesByDate(dateStart: String, dateFinish: String) : LiveData<List<NoteDomain>>
 }
