3330a809444d09746790757fc73f0904815fbb7d
Dinar Subeev <dinars163@gmail.com>
2024-01-10 03:32:41 +0400

    Changed method signatures to the required ones, the logic and mappers are written

--- app/src/main/java/com/ggr3ml1n/notesmanager/data/repositories/NoteRepositoryImpl.kt
+++ app/src/main/java/com/ggr3ml1n/notesmanager/data/repositories/NoteRepositoryImpl.kt
@@ -1,23 +1,52 @@
 package com.ggr3ml1n.notesmanager.data.repositories
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.ggr3ml1n.notesmanager.data.entities.NoteData
 import com.ggr3ml1n.notesmanager.data.storage.NoteStorage
+import com.ggr3ml1n.notesmanager.domain.entities.NoteDomain
 import com.ggr3ml1n.notesmanager.domain.repository.NoteRepository
 
 class NoteRepositoryImpl(private val noteStorage: NoteStorage) : NoteRepository {
 
-    override suspend fun deleteNote() {
-        noteStorage.deleteNote()
+    override suspend fun deleteNote(id: Int) {
+        noteStorage.deleteNote(id = id)
     }
 
-    override suspend fun saveNote() {
-        noteStorage.saveNote()
+    override suspend fun saveNote(note: NoteDomain) {
+        noteStorage.saveNote(toData(note))
     }
 
-    override suspend fun updateNote() {
-        noteStorage.updateNote()
+    override suspend fun updateNote(note: NoteDomain) {
+        noteStorage.saveNote(toData(note))
     }
 
-    override suspend fun getNotesByDate() {
-        noteStorage.getNotesByDate()
+    override fun getNotesByDate(dateStart: String, dateFinish: String) : LiveData<List<NoteDomain>> {
+        val dataLiveData = noteStorage.getNotesByDate(
+            dateStart = dateStart,
+            dateFinish = dateFinish
+        ).value
+            ?.map { toDomain(it) }
+        return MutableLiveData(dataLiveData)
+    }
+
+    private fun toDomain(note: NoteData): NoteDomain {
+        return NoteDomain(
+            id = note.id,
+            dateStart = note.dateStart,
+            dateFinish = note.dateFinish,
+            name = note.name,
+            description = note.description
+        )
+    }
+
+    private fun toData(note: NoteDomain): NoteData {
+        return NoteData(
+            id = note.id,
+            dateStart = note.dateStart,
+            dateFinish = note.dateFinish,
+            name = note.name,
+            description = note.description
+        )
     }
 }
