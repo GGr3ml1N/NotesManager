1d82098328a07c02e5ffbfa2b608766f81d9a7df
Dinar Subeev <dinars163@gmail.com>
2024-01-09 23:37:54 +0400

    Created MainDataBase

---
+++ app/src/main/java/com/ggr3ml1n/notesmanager/data/MainDataBase.kt
@@ -0,0 +1,29 @@
+package com.ggr3ml1n.notesmanager.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [NoteData::class], version = 1)
+abstract class MainDataBase : RoomDatabase(){
+
+    abstract fun getDAO(): DAO
+
+    companion object{
+        @Volatile
+        private var INSTANCE: MainDataBase? = null
+
+        fun getDataBase(context: Context): MainDataBase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MainDataBase::class.java,
+                    "NotesManager.db")
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
