75cd05ed94e59e4d56e273dbc8e6a06497debe32
Dinar Subeev <dinars163@gmail.com>
2024-01-10 16:23:01 +0400

    Changed name of usecase

--- domain/src/main/java/com/ggr3ml1n/domain/usecases/GetNotesByDateUseCase.kt
+++ domain/src/main/java/com/ggr3ml1n/domain/usecases/GetNotesByDateUseCase.kt
@@ -3,7 +3,7 @@ package com.ggr3ml1n.domain.usecases
 import com.ggr3ml1n.domain.entities.NoteDomain
 import com.ggr3ml1n.domain.repository.NoteRepository
 
-class GetNotesByPageUseCase(private val noteRepository: NoteRepository) {
+class GetNotesByDateUseCase(private val noteRepository: NoteRepository) {
 
     fun execute(note: NoteDomain): List<NoteDomain> {
         return noteRepository.getNotesByDate(
