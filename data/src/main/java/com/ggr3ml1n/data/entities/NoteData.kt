251482d6ee3d368a4d7fbae9c646471f9323bfe0
Dinar Subeev <dinars163@gmail.com>
2024-01-17 20:27:05 +0400

    Changed database name. dateFinish and dateStart type changed.

--- data/src/main/java/com/ggr3ml1n/data/entities/NoteData.kt
+++ data/src/main/java/com/ggr3ml1n/data/entities/NoteData.kt
@@ -4,11 +4,11 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Notes")
+@Entity(tableName = "notes")
 data class NoteData(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "dateStart") val dateStart: String,
-    @ColumnInfo(name = "dateFinish") val dateFinish: String,
+    @PrimaryKey(autoGenerate = true) val id: Int?,
+    @ColumnInfo(name = "dateStart") val dateStart: Long,
+    @ColumnInfo(name = "dateFinish") val dateFinish: Long,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "description") val description: String,
 )
