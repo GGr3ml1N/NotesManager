0dc50e1aefd9318c6909344f69c150cc90fcd0ff
Dinar Subeev <dinars163@gmail.com>
2024-01-17 19:38:31 +0400

    dateAndTime -> date

--- app/src/main/java/com/ggr3ml1n/notesmanager/presentation/app/fragments/AllNotesFragment.kt
+++ app/src/main/java/com/ggr3ml1n/notesmanager/presentation/app/fragments/AllNotesFragment.kt
@@ -55,7 +55,7 @@ class AllNotesFragment : Fragment() {
                 .apply {
                     putExtra(
                         DATA,
-                        vm.dateAndTime
+                        vm.date
                     )
                 }
             )
