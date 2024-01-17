c2b70e946a2368b2d6143bcc6d9b783b8c506683
Dinar Subeev <dinars163@gmail.com>
2024-01-17 20:27:19 +0400

    id nullable

--- domain/src/main/java/com/ggr3ml1n/domain/entities/NoteDomain.kt
+++ domain/src/main/java/com/ggr3ml1n/domain/entities/NoteDomain.kt
@@ -3,7 +3,7 @@ package com.ggr3ml1n.domain.entities
 import java.sql.Timestamp
 
 data class NoteDomain(
-    val id: Int,
+    val id: Int?,
     val dateStart: Timestamp,
     val dateFinish: Timestamp,
     val name: String,
