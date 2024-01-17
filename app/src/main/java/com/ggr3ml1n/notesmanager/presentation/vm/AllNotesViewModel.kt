0237c1f8e187da06beb74ae751ba2ab70091250d
Dinar Subeev <dinars163@gmail.com>
2024-01-17 19:38:02 +0400

    The "init" method has been removed. The DateAndTime field has been renamed to date. Its parameters have been changed, a private set has been declared. Changed the onLongClick method.

--- app/src/main/java/com/ggr3ml1n/notesmanager/presentation/vm/AllNotesViewModel.kt
+++ app/src/main/java/com/ggr3ml1n/notesmanager/presentation/vm/AllNotesViewModel.kt
@@ -3,50 +3,48 @@ package com.ggr3ml1n.notesmanager.presentation.vm
 import android.app.DatePickerDialog
 import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import com.ggr3ml1n.domain.entities.NoteDomain
 import com.ggr3ml1n.domain.usecases.GetNotesByDateUseCase
-import kotlinx.coroutines.launch
 import java.util.Calendar
 
 class AllNotesViewModel(
-    private val getNotesByDateUseCase: GetNotesByDateUseCase
+    getNotesByDateUseCase: GetNotesByDateUseCase
 ) : ViewModel() {
 
-    private val _listOfNotes: MutableLiveData<List<NoteDomain>> = MutableLiveData()
-    val listOfNotes: LiveData<List<NoteDomain>> = _listOfNotes
-
-    private var dateAndTime: Calendar = Calendar.getInstance()
-
-    fun init() {
-        viewModelScope.launch {
-            _listOfNotes.value = getNotesByDateUseCase
-                .execute(dateAndTime.timeInMillis.toString())
-                .asLiveData().value
-        }
+    var date: Calendar = Calendar.getInstance().apply {
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
     }
+        private set
+
+    val listOfNotes: LiveData<List<NoteDomain>> = getNotesByDateUseCase.execute(date.timeInMillis.toString()).asLiveData()
 
     fun datePickerDialog(context: Context) {
         DatePickerDialog(
             context,
-            listener(),
-            dateAndTime[Calendar.YEAR],
-            dateAndTime[Calendar.MONTH],
-            dateAndTime[Calendar.DAY_OF_MONTH],
+            listenerDate(),
+            date[Calendar.YEAR],
+            date[Calendar.MONTH],
+            date[Calendar.DAY_OF_MONTH],
         ).show()
     }
 
-    private fun listener(): DatePickerDialog.OnDateSetListener {
+    private fun listenerDate(): DatePickerDialog.OnDateSetListener {
         return DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
-            dateAndTime.set(year, month, dayOfMonth)
-            init()
+            date.set(year, month, dayOfMonth)
         }
     }
 
-    fun onLongClick(){
-        dateAndTime = Calendar.getInstance()
+    fun onLongClick() {
+        date = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
     }
 }
