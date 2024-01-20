c7963785d5c676937023a7c2edfb7e1348db4b64
Dinar Subeev <dinars163@gmail.com>
2024-01-20 18:37:12 +0400

    Serializable to pass between activities/fragment

--- domain/src/main/java/com/ggr3ml1n/domain/entities/NoteDomain.kt
+++ domain/src/main/java/com/ggr3ml1n/domain/entities/NoteDomain.kt
@@ -1,5 +1,6 @@
 package com.ggr3ml1n.domain.entities
 
+import java.io.Serializable
 import java.sql.Timestamp
 
 data class NoteDomain(
@@ -8,4 +9,4 @@ data class NoteDomain(
     val dateFinish: Timestamp,
     val name: String,
     val description: String,
-)
+) : Serializable
